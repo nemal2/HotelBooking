c55981f9f1a6cc8136bf2b1f00c2ea6990b3b5d2
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2025-03-29 18:11:57 +0530

    latest update with server connection

--- hotelBackend/src/main/java/com/nemal/hotelBackend/hotel/config/WebConfig.java
+++ hotelBackend/src/main/java/com/nemal/hotelBackend/hotel/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Your frontend's URL
+                        .allowedOrigins("*") // Your frontend's URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
