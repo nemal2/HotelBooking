ef2bacbcbc4891fe264c5521c79f13e6ba924945
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2025-03-29 18:49:26 +0530

    latest update with server connection

--- hotelBackend/src/main/java/com/example/hotelbackend/hotel/repositories/RoomTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hotelbackend.hotel.repositories;
-
-import com.example.hotelbackend.hotel.model.RoomType;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomTypeRepository extends MongoRepository<RoomType, String> {
-}
--- hotelBackend/src/main/java/com/example/hotelbackend/hotel/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hotelbackend.hotel.repositories;
-
-import com.example.hotelbackend.hotel.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-    boolean existsByEmail(String email);
-}
